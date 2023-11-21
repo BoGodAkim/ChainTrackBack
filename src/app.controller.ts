10d307a8c52ee847b3de4893046d7b109dc651d5
Akim Bahadziazh <56846337+BoGodAkim@users.noreply.github.com>
2023-11-21 17:13:34 +0100

    Fix error handling in default case

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -73,9 +73,10 @@ export class AppController {
           break;
         }
         default: {
-          return req.session.save(() =>
-            res.status(500).json({ message: e.message }),
-          );
+          return res.status(500).json({ message: e.message });
+          // return req.session.save(() =>
+          //   res.status(500).json({ message: e.message }),
+          // );
           break;
         }
       }
